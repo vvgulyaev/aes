48485c4b2a4c26acffe5ffb1d0b1be2be83ef6e6
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2020-07-31 15:47:55 +0300

    Updated test bench for standalon simulation of encoder. Implemented using text files for encoding testing

--- tb/tb_fpga.sv
+++ tb/tb_fpga.sv
@@ -1,3 +1,13 @@
+/**
+ * Testbench for simulation of AES-128-ECB encoder in fpga system using Xilinx IP UARTLITE
+ *
+ *  Copyright 2020 by Vyacheslav Gulyaev <v.gulyaev181@gmail.com>
+ *
+ *  Licensed under GNU General Public License 3.0 or later. 
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
 
 module tb_fpga ();
     
